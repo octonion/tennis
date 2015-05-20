aefa1d6c1bc28b793602810bfeb27181cbd06700
Christopher D. Long <galizur@gmail.com>
2015-05-20 03:22:02 -0400

    NCAA men's singles rankings.

---
+++ sql/men_singles.sql
@@ -0,0 +1,13 @@
+copy (
+select
+row_number() over (order by estimate desc) as rk,
+player_name as player,
+team_name as team,
+exp(estimate)::numeric(6,2) as str
+from ita.sm_basic_factors bf
+join ita.players p
+on (p.player_id)=(bf.level::integer)
+where bf.factor='player'
+order by str desc
+) to '/tmp/men_singles.csv' csv header;
+
