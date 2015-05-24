3890224caca03d01aaa62a9884ef435bdd58a84d
Christopher D. Long <galizur@gmail.com>
2015-05-24 00:18:57 -0400

    NCAA women's singles tournament.

---
+++ women_singles_tournament/round_p.sql
@@ -0,0 +1,25 @@
+begin;
+
+select round_id as rd,
+r.player_name as name,
+r.team_name as team,
+p::numeric(4,3) as p
+from ita.women_singles_rounds r
+where TRUE --round_id=2
+and p<1.0
+order by round_id asc,p desc;
+
+copy
+(
+select round_id as rd,
+r.player_name as name,
+r.team_name as team,
+p::numeric(4,3) as p
+from ita.women_singles_rounds r
+where TRUE --round_id=2
+and p<1.0
+order by round_id asc,p desc
+)
+to '/tmp/round_p.csv' csv header;
+
+commit;
---
+++ women_singles_tournament/update_round.sql
@@ -0,0 +1,36 @@
+begin;
+
+insert into ita.women_singles_rounds
+(year,round_id,division_id,player_id,player_name,team_name,bracket,p)
+(
+select
+r1.year as year,
+r1.round_id+1 as round,
+r1.division_id,
+r1.player_id,
+r1.player_name,
+r1.team_name,
+r1.bracket,
+
+sum(
+(case when r2.player_id is null then 1.0
+      else r1.p*r2.p*mp.player_p
+ end)
+) as p
+
+from ita.women_singles_rounds r1
+left join ita.women_singles_rounds r2
+  on ((r1.year,r1.round_id,r1.bracket[r1.round_id+1])=
+      (r2.year,r2.round_id,r2.bracket[r1.round_id+1])
+       and not(r1.bracket[r1.round_id]=r2.bracket[r1.round_id]))
+left join ita.women_singles_matrix_p mp
+  on (mp.year,mp.player_id,mp.opponent_id)=
+     (r1.year,r1.player_id,r2.player_id)
+
+where
+    r1.year=2015
+and r1.round_id=1
+group by r1.year,round,r1.division_id,r1.player_id,r1.player_name,r1.team_name,r1.bracket
+);
+
+commit;
