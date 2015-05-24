f1e12a49652ca4355bc2dc573de31d0475183df9
Christopher D. Long <galizur@gmail.com>
2015-05-24 16:20:05 -0400

    French Open 2015 point-by-point and speed data update.

---
+++ slams/tournament.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'csv'
+require 'mechanize'
+require 'json'
+
+agent = Mechanize.new{ |agent| agent.history.max_size=0 }
+
+agent.user_agent = 'Mozilla/5.0'
+
+year = 2015
+
+tn = "rolandgarros"
+
+url = "http://2015.rolandgarros.com/en_FR/xml/gen/apps/cmatch/eventDays.json"
+  
+page = agent.get(url).body
+j = JSON.parse(page)
+fn = url.split("/")[-1]
+
+file_name = "#{tn}/#{year}/#{fn}"
+p file_name
+
+File.write("#{tn}/#{year}/#{fn}", j.to_json)
